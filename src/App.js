bf26eb71c81d04f98772ac4ff39954e8c04ec8f8
Bogdan Lobor <b.lobor@outlook.com>
2016-04-10 15:24:46 +0300

    Adding functionality for CardNumber component.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,43 @@ import React from 'react';
 import bukovelAPI from './dataAccess/bukovelApi';
 import CardNumberInput from './CardNumberInput';
 
+const TEST_CARD_NUMBER = '01-2167-30-92545';
+
 export default class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      html: ''
+      html: '',
+      cardNumber: TEST_CARD_NUMBER
+    };
+    this.handleCardNumberChange = this.handleCardNumberChange.bind(this);
+  }
+
+  // componentWillMount() {
+  //   setTimeout(() => {
+  //     this.setState({
+  //       cardNumber: TEST_CARD_NUMBER
+  //     })
+  //   })
+  // }
+
+  handleCardNumberChange(event) {
+    if (event.isValid) {
+      bukovelAPI
+        .getCardBalance(event.text)
+        .then(data => {
+          this.setState({
+            html: JSON.stringify(data, null, '\t')
+          });
+        })
     }
   }
-  componentDidMount() {
-    bukovelAPI
-      .getCardBalance()
-      .then(data => {
-        this.setState({
-          html: JSON.stringify(data, null, '\t')
-        });
-      })
-  }
+
   render() {
     return (
       <div>
-        <CardNumberInput/>
+        <CardNumberInput onChange={this.handleCardNumberChange} value={this.state.cardNumber} />
         <pre>{this.state.html}</pre>
       </div>
     );
--- src/CardNumberInput.js
+++ src/CardNumberInput.js
@@ -1,49 +1,124 @@
 import React from 'react';
 import InputMask from 'inputmask-core';
 
+const ENTER_KEY = 'Enter';
+const BACKSPACE_KEY = 'Backspace';
+
 class CardNumberInput extends React.Component {
   constructor() {
     super();
 
+    this.state = {
+      isValid: false,
+      text: ''
+    }
+
+    this.handlePaste = this.handlePaste.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
   componentWillMount() {
+    const selection = this.props.value ? this.props.value.length : 0;
     let options = {
       pattern: '11-1111-11-11111',
-      placeholderChar: ' '
+      placeholderChar: ' ',
+      value: this.props.value,
+      selection: {
+        start: selection,
+        end: selection
+      }
     }
-
     this.mask = new InputMask(options);
+
+    const value = getDisplayMaskValue(this.mask);
+    this.setState({
+      text: value,
+      isValid: isValidValue(value, this.mask)
+    }, () => triggerOnchange(this.state, this.props));
   }
 
-  handleChange(event) {
-    let value = event.target.value;
+  componentWillReceiveProps(nextProps) {
+    if (this.props.value !== nextProps.value) {
+      const selection = nextProps.value ? nextProps.value.length : 0;
 
-    if (this.props.update) {
-      this.props.update(value);
+      this.mask.setValue(nextProps.value);
+      this.mask.setSelection({
+        start: selection,
+        end: selection
+      })
+
+      const value = getDisplayMaskValue(this.mask);
+      this.setState({
+        text: value,
+        isValid: isValidValue(value, this.mask)
+      }, () => triggerOnchange(this.state, this.props));
+    }
+  }
+  
+  handleChange(event) {
+
+    const maskValue = this.mask.getValue()
+    const elementValue = event.target.value;
+    if (elementValue !== maskValue) {
+      // Cut or delete operations will have shortened the value
+      if (elementValue.length < maskValue.length) {
+        const sizeDiff = maskValue.length - elementValue.length
+        this.mask.setSelection(getElementSelection(event.target));
+        this.mask.selection.end = this.mask.selection.start + sizeDiff
+        this.mask.backspace()
+      }
+      const value = getDisplayMaskValue(this.mask);
+      this.setState({
+        text: value,
+        isValid: isValidValue(value, this.mask)
+      }, () => triggerOnchange(this.state, this.props));
+    }
+  }
+
+  handlePaste(event) {
+    event.preventDefault();
+
+    this.mask.setSelection(getElementSelection(event.target));
+    if (this.mask.paste(event.clipboardData.getData('text'))) {
+      const value = getDisplayMaskValue(this.mask);
+      this.setState({
+        text: value,
+        isValid: isValidValue(value, this.mask)
+      }, () => triggerOnchange(this.state, this.props));
     }
   }
 
   handleKeyDown(event) {
-    if (event.key === 'Backspace') {
+    if (event.key === BACKSPACE_KEY) {
       event.preventDefault();
 
-      this.mask.selection = getElementSelection(event.target);
+      this.mask.setSelection(getElementSelection(event.target));
       if (this.mask.backspace()) {
-        event.target.value = getDisplayMaskValue(this.mask);
+        const value = getDisplayMaskValue(this.mask);
+        this.setState({
+          text: value,
+          isValid: isValidValue(value, this.mask)
+        }, () => triggerOnchange(this.state, this.props));
       }
     }
   }
 
   handleKeyPress(event) {
+    if (event.metaKey || event.altKey || event.ctrlKey || event.key === ENTER_KEY) {
+      return
+    }
+
     event.preventDefault();
 
-    this.mask.selection = getElementSelection(event.target);
+    this.mask.setSelection(getElementSelection(event.target));
     if (this.mask.input(event.key)) {
-      event.target.value = getDisplayMaskValue(this.mask);
+      const value = getDisplayMaskValue(this.mask);
+      this.setState({
+        text: value,
+        isValid: isValidValue(value, this.mask)
+      }, () => triggerOnchange(this.state, this.props));
     }
   }
 
@@ -51,14 +126,17 @@ class CardNumberInput extends React.Component {
     return <input type="text"
       size={this.mask.pattern.length}
       maxLength={this.mask.pattern.length}
+      onChange={this.handleChange}
+      onPaste={this.handlePaste}
       onKeyDown={this.handleKeyDown}
       onKeyPress={this.handleKeyPress}
-      onChange={this.handleChange} />
+      value={this.state.text} />
   }
 }
 
 CardNumberInput.propTypes = {
-  update: React.PropTypes.func
+  value: React.PropTypes.string,
+  onChange: React.PropTypes.func
 }
 
 function getElementSelection(element) {
@@ -72,6 +150,21 @@ function getDisplayMaskValue(mask) {
   return mask
     .getValue()
     .slice(0, mask.selection.end);
+}
+
+function isValidValue(value, mask) {
+  return value &&
+    value !== mask.emptyValue &&
+    value.length === mask.pattern.length;
+}
+
+function triggerOnchange(state, props) {
+  if (props.onChange) {
+    props.onChange({
+      text: state.text,
+      isValid: state.isValid
+    });
+  }
 }
 
 export default CardNumberInput;
