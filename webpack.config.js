2bcbff53f470c418bbe4290c243abf041afb7089
Bogdan Lobor <b.lobor@outlook.com>
2016-04-12 18:16:35 +0300

    Configure style loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,44 @@
 'use strict';
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const sassLoaders = [
+  'css-loader?sourceMap',
+  'sass-loader?outputStyle=expanded'
+];
+
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: './',
-        filename: 'bundle.js'
-    },
-    devtool: '#source-map',
-    devServer: {
-        port: 3333,
-        inline: true
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                  presets: ['react', 'babel-preset-es2015']
-                }
-            },
-            {
-                test: /\.json?$/,
-                loader: 'json'
-            }
-        ]
-    }
+  entry: './src/main.js',
+  output: {
+    path: './',
+    filename: 'bundle.js'
+  },
+  devtool: '#source-map',
+  devServer: {
+    port: 3333,
+    inline: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'babel-preset-es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', sassLoaders)
+      },
+      {
+        test: /\.json?$/,
+        loader: 'json'
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("bundle.css")
+  ]
 }
