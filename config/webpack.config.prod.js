baeabeb91aac94525230be87812dc7d951615601
Bogdan Lobor <b.lobor@outlook.com>
2016-04-17 16:43:47 +0300

    Injecting a variable process.env.NODE_ENV set to "production" at build time.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -42,6 +42,11 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new webpack.NoErrorsPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.DedupePlugin(),
