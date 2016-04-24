d1761947f6d685524816d54a305ffd800681d2b0
Bogdan Lobor <b.lobor@outlook.com>
2016-04-24 11:52:50 +0300

    Update material-ui library. Use react-tap-event-plugin.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import 'normalize.css';
 import './styles/styles.scss';
 
 import 'babel-polyfill';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+injectTapEventPlugin();
 
 ReactDOM.render(<App/>, document.getElementById('buka-app'));
