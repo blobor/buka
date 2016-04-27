7972fe84cb18da5343df4885f72fe9da0a5d3ddf
Bogdan Lobor <b.lobor@outlook.com>
2016-04-27 19:09:43 +0300

    Add comment.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
 injectTapEventPlugin();
 
 ReactDOM.render(<App/>, document.getElementById('buka-app'));
