00110b2526c901cca72af3038aa19524653fa23a
Bogdan Lobor <b.lobor@outlook.com>
2016-05-10 18:27:35 +0300

    move server.js file into "src" folder.

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const PATH = require('path');
+
+const express = require('express');
+const app = express();
+
+const ROOT = '../';
+
+const port = process.env.PORT || 3333;
+const staticFolder = PATH.resolve(__dirname, ROOT, 'dist');
+
+// don't send "X-Powered By" header
+app.disable('x-powered-by');
+app.use(express.static(staticFolder));
+
+app.listen(port, function() {
+  console.log(`Express server listening on port ${port}`); // eslint-disable-line no-console
+  console.log(`env = ${app.get('env')}`); // eslint-disable-line no-console
+  console.log(`__dirname = ${__dirname}`); // eslint-disable-line no-console
+  console.log(`staticFolder = ${staticFolder}`); // eslint-disable-line no-console
+});
