ac152beb92a67ab956697476f25d8ff46840f433
Bogdan Lobor <b.lobor@outlook.com>
2016-06-11 07:54:14 +0300

    added redux logging middleware

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
+const middlewares = [];
+
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(createLogger());
+}
+
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState, applyMiddleware(...middlewares));
 }
