26da4c034635b129dc3005eed2f7ab4316039557
Bogdan Lobor <b.lobor@outlook.com>
2016-06-14 20:36:16 +0300

    move actionTypes into separate file.

--- src/app/actions/skipass.js
+++ src/app/actions/skipass.js
@@ -1,25 +1,22 @@
 import bukovelAPI from '../data-access/bukovelAPI'
-
-export const CHANGE_CARD_NUMBER = 'CHANGE_CARD_NUMBER'
-export const REQUEST_SKIPASS_DATA = 'REQUEST_SKIPASS_DATA'
-export const RECEIVE_SKIPASS_DATA = 'RECEIVE_SKIPASS_DATA'
+import * as actionTypes from './actionTypes'
 
 const changeCardNumber = (cardNumber) => {
   return {
-    type: CHANGE_CARD_NUMBER,
+    type: actionTypes.CHANGE_CARD_NUMBER,
     cardNumber: cardNumber
   }
 }
 
 const requestSkipassData = () => {
   return {
-    type: REQUEST_SKIPASS_DATA
+    type: actionTypes.REQUEST_SKIPASS_DATA
   }
 }
 
 const receiveSkipassdata = (skipass) => {
   return {
-    type: RECEIVE_SKIPASS_DATA,
+    type: actionTypes.RECEIVE_SKIPASS_DATA,
     skipass: skipass
   }
 }
--- src/app/reducers/skipass.js
+++ src/app/reducers/skipass.js
@@ -1,5 +1,4 @@
-import { CHANGE_CARD_NUMBER,
-         REQUEST_SKIPASS_DATA, RECEIVE_SKIPASS_DATA } from '../actions/skipass'
+import * as actionTypes from '../actions/actionTypes'
 
 const initialState = {
   cardNumber: '',
@@ -9,17 +8,17 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case CHANGE_CARD_NUMBER:
+    case actionTypes.CHANGE_CARD_NUMBER:
       return {
         ...state,
         cardNumber: action.cardNumber
       }
-    case REQUEST_SKIPASS_DATA:
+    case actionTypes.REQUEST_SKIPASS_DATA:
       return {
         ...state,
         isFetching: true
       }
-    case RECEIVE_SKIPASS_DATA:
+    case actionTypes.RECEIVE_SKIPASS_DATA:
       return {
         ...state,
         ...action.skipass,
