b9afc3bd471ed0d64c0598702a682ef781ec0ae7
Bogdan Lobor <b.lobor@outlook.com>
2016-09-04 01:02:48 +0300

    added test for `bukovel-shop` parser

--- test/bukovel-shop-parser.js
+++ test/bukovel-shop-parser.js
@@ -1,5 +1,6 @@
 import test from 'ava'
 import pify from 'pify'
+import has from 'lodash.has'
 import { readFile } from 'fs'
 
 import { parse } from '../src/server/parsers/bukovel-shop'
@@ -16,4 +17,24 @@ test('should parse response', t => {
       t.is(data.cardNumber, '01-2167-13-185238')
       t.is(data.lifts.length, 14)
     })
+})
+
+test('in case of error response parser should return array of errors values', t => {
+  // Arrange
+  const error1Message1 = 'error_1 message'
+  const error2Message2 = 'error_2 message'
+  const data = {
+    error: {
+      '1': error1Message1,
+      '2': error2Message2
+    }
+  }
+
+  // Act
+  const parsedData = parse(data)
+
+  // Assert
+  t.true(has(parsedData, 'errors'))
+  t.true(Array.isArray(parsedData.errors))
+  t.deepEqual(parsedData.errors, [error1Message1, error2Message2])
 })
