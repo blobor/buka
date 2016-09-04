ecfc57007adcff543d3ad7addb0f6e84e9d7a00d
Bogdan Lobor <b.lobor@outlook.com>
2016-09-04 13:16:59 +0300

    minor `bukovel-ticket parser` refactor

--- src/server/parsers/bukovel-ticket.js
+++ src/server/parsers/bukovel-ticket.js
@@ -1,10 +1,17 @@
 import cheerio from 'cheerio'
+import get from 'lodash.get'
+import isNil from 'lodash.isnil'
 import { parse } from 'qs'
 
 export const parseCardNumber = html => {
   const $ = cheerio.load(html)
 
-  const cardNumberHref = $('#result tr:nth-child(3) a').attr('href')
+  const cardNumberHref = $('#result')
+    .find('tr:nth-child(3) a').attr('href')
 
-  return parse(cardNumberHref).Card
+  if (isNil(cardNumberHref)) {
+    return null
+  }
+
+  return get(parse(cardNumberHref), 'Card', null)
 }
