bc9f8edae9be0a7d8c9119819fc37d3990896cd6
Bogdan Lobor <b.lobor@outlook.com>
2016-09-04 13:17:59 +0300

    added `getSkipass` function

--- src/server/data-sourses/bukovel-tickets/bukovel.js
+++ src/server/data-sourses/bukovel-tickets/bukovel.js
@@ -4,19 +4,32 @@ import fetch from 'node-fetch'
 import { parseCardNumber } from '../../parsers/bukovel-ticket'
 
 const BUKOVEL_TICKETS_URL = 'http://tickets.bukovel.com/'
+const getResponseText = response => {
+  if (!response.ok) {
+    return Promise.reject('Response is not ok')
+  }
+
+  return response.text()
+}
 
 export const getSkipassCardNumber = id => {
   const params = {
-    'NumTicket': id
+    NumTicket: id
   }
 
   return fetch(`${BUKOVEL_TICKETS_URL}?${stringify(params)}`)
-    .then(response => {
-      if (!response.ok) {
-        return Promise.reject('Response is not ok')
-      }
-
-      return response.text()
-    })
+    .then(getResponseText)
     .then(parseCardNumber)
+}
+
+export const getSkipass = async id => {
+  const cardNumber = await getSkipassCardNumber(id)
+
+  const params = {
+    NumTicket: id,
+    Card: cardNumber
+  }
+
+  return fetch(`${BUKOVEL_TICKETS_URL}?${stringify(params)}`)
+    .then(getResponseText)
 }
