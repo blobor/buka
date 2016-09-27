c73d4d9c232734f0d38cf8c71765369de6cb995a
Bogdan Lobor <b.lobor@outlook.com>
2016-09-27 09:08:21 +0300

    added `localstorage` and `memory` adapters for PouchDB

--- src/app/data-access/PouchDB-client.js
+++ src/app/data-access/PouchDB-client.js
@@ -1,9 +1,13 @@
 import PouchDBCore from 'pouchdb-core'
 import idbApapter from 'pouchdb-adapter-idb'
 import websqlApapter from 'pouchdb-adapter-websql'
+import localstorageApapter from 'pouchdb-adapter-localstorage'
+import memorypapter from 'pouchdb-adapter-memory'
 
 const PouchDB = PouchDBCore
   .plugin(idbApapter)
   .plugin(websqlApapter)
+  .plugin(localstorageApapter)
+  .plugin(memorypapter)
 
 export default PouchDB
