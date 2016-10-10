9635a59bd91c4b93f7ccc8fa561e2855c2e3fa44
Bogdan Lobor <b.lobor@outlook.com>
2016-10-10 13:14:35 +0300

    :arrow_up: deps
    fix breaking changes in `materia-ui`

--- src/app/components/SkipassSearchResult.js
+++ src/app/components/SkipassSearchResult.js
@@ -6,7 +6,7 @@ import CircularProgress from 'material-ui/CircularProgress'
 
 const SkipassSearchResult = ({ search }) => {
   if (search.isFetching) {
-    return <CircularProgress size={1.5} />
+    return <CircularProgress size={90} thickness={5} />
   } else if (search.error) {
     return <p>{search.error}</p>
   } else if (isNil(search.skipass)) {
