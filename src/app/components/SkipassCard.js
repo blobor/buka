9c684c3ea8dd0ef137df581e2ad3428377fc18e1
Bogdan Lobor <b.lobor@outlook.com>
2016-10-16 23:43:56 +0300

    added animated state for skipass card

--- src/app/components/SkipassCard.js
+++ src/app/components/SkipassCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SkipassCard = ({ skipass }) => {
   return (
-    <article className='skipass-card skipass-card--shadow'>
+    <article className='skipass-card skipass-card--shadow skipass-card skipass-card--shadow-animated'>
       <header className='skipass-card__header'>
         <h5 className='skipass-card__title'>{skipass.cardNumber}</h5>
       </header>
