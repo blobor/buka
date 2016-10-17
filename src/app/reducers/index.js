a34a5d0799351696d7995c95f49273c521e0c335
Bogdan Lobor <b.lobor@outlook.com>
2016-10-17 11:11:15 +0300

    rename searchSkipass.js -> search-skipass.js

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux-immutable'
 import app from './app'
-import searchSkipass from './searchSkipass'
+import searchSkipass from './search-skipass'
 import storedSkipasses from './stored-skipasses'
 
 const rootReducer = combineReducers({
