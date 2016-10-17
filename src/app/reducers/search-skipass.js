4f54d8522a6121a76d5ad7a20bbf9ea68d7bdb9a
Bogdan Lobor <b.lobor@outlook.com>
2016-10-17 22:56:23 +0300

    import only used actions

--- src/app/reducers/search-skipass.js
+++ src/app/reducers/search-skipass.js
@@ -1,5 +1,12 @@
 import { fromJS } from 'immutable'
-import * as actionTypes from '../actions/actionTypes'
+import {
+  CHANGE_SEARCH_SKIPASS_NUMBER,
+  TOGGLE_SKIPASS_CAN_BE_ADDED,
+  FETCH_SKIPASS_REQUEST,
+  FETCH_SKIPASS_SUCCESS,
+  FETCH_SKIPASS_FAILURE,
+  STORE_SKIPASS_SUCCESS
+} from '../actions/actionTypes'
 
 const initialState = fromJS({
   isValid: true,
@@ -10,32 +17,32 @@ const initialState = fromJS({
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.CHANGE_SEARCH_SKIPASS_NUMBER:
+    case CHANGE_SEARCH_SKIPASS_NUMBER:
       return state.merge({
         skipassNumber: action.skipassNumber,
         isValid: action.isValid
       })
-    case actionTypes.TOGGLE_SKIPASS_CAN_BE_ADDED:
+    case TOGGLE_SKIPASS_CAN_BE_ADDED:
       return state.set('canBeAdded', action.canBeAdded)
-    case actionTypes.FETCH_SKIPASS_REQUEST:
+    case FETCH_SKIPASS_REQUEST:
       return state.merge({
         isFetching: true,
         canBeAdded: false
       })
-    case actionTypes.FETCH_SKIPASS_SUCCESS:
+    case FETCH_SKIPASS_SUCCESS:
       return state.merge({
         isFetching: false,
         error: null,
         skipass: action.skipass
       })
-    case actionTypes.FETCH_SKIPASS_FAILURE:
+    case FETCH_SKIPASS_FAILURE:
       return state.merge({
         isFetching: false,
         canBeAdded: false,
         skipass: null,
         error: action.error
       })
-    case actionTypes.STORE_SKIPASS_SUCCESS:
+    case STORE_SKIPASS_SUCCESS:
       return state.set('canBeAdded', false)
     default:
       return state
