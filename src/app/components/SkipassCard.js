7212390f67f9682500f2a5fb45f4d9b7e039148a
Bogdan Lobor <b.lobor@outlook.com>
2016-10-22 17:34:27 +0300

    continue work on skipass cards

--- src/app/components/SkipassCard.js
+++ src/app/components/SkipassCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { getAdoptedDateString } from '../helpers/date'
 
 const SkipassCard = ({ skipass }) => {
   return (
@@ -6,7 +7,16 @@ const SkipassCard = ({ skipass }) => {
       <header className='skipass-card__header'>
         <h5 className='skipass-card__title'>{skipass.name}</h5>
       </header>
-      <main className='skipass-card__main-container' />
+      <main className='skipass-card__main-container'>
+        <h5 className='skipass-card__card-number'>{skipass.cardNumber}</h5>
+        <section className='skipass-card__info-container'>
+          <time className='skipass-card__purchase-date' dateTime={skipass.purchaseDate}>{ getAdoptedDateString(skipass.purchaseDate) }</time>
+        </section>
+        <section className='skipass-card__balance-container'>
+          <h5 className='skipass-card__balance-title'>Balance</h5>
+          <span className='skipass-card__balance'>0</span>
+        </section>
+      </main>
     </article>
   )
 }
