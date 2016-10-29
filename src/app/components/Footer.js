9ecbd77db3524399dcd0d04f2a3ce69b17322d9a
Bogdan Lobor <b.lobor@outlook.com>
2016-10-30 02:58:10 +0300

    added `BottomNavigationLink` component

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,23 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router'
-import { BottomNavigationItem } from 'material-ui/BottomNavigation'
 import { ActionSearch, ContentSave } from 'material-ui/svg-icons'
+import BottomNavigationLink from './BottomNavigationLink'
 
 const Footer = () => {
   return (
     <footer className='buka__footer material-shadow--z1'>
-      <Link to='/'>
-        <BottomNavigationItem
+      <nav className='bottom-navigation'>
+        <BottomNavigationLink
+          to='/'
           label='Search'
-          icon={<ActionSearch />}
-        />
-      </Link>
-      <Link to='/skipasses'>
-        <BottomNavigationItem
+          activeOnlyWhenExact
+          icon={
+            <ActionSearch />
+          } />
+        <BottomNavigationLink
+          to='/skipasses'
           label='Skipasses'
-          icon={<ContentSave />}
-        />
-      </Link>
+          activeOnlyWhenExact
+          icon={
+            <ContentSave />
+          } />
+      </nav>
     </footer>
   )
 }
