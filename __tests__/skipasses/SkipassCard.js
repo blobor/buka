ff9975d60252ba273ea141b905041b89ff14291f
Bogdan Lobor <b.lobor@outlook.com>
2017-01-11 06:34:57 +0200

    [gh-49]: pass timezone free date

--- __tests__/skipasses/SkipassCard.js
+++ __tests__/skipasses/SkipassCard.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 
 import React from 'react'
+import moment from 'moment'
 import renderer from 'react-test-renderer'
 import { MuiThemeProvider } from 'material-ui/styles'
 import { getMuiTheme } from '../../src/app/core/mui-theme'
@@ -12,12 +13,12 @@ describe('SkipassCard', () => {
       cardNumber: '01-2167-30-92545',
       ticketNumber: '01-1614 7133 5345 3531 0476-4',
       name: '5 днів СЕЗОН (ВСЕF)',
-      purchaseDate: new Date('2015-03-09'),
+      purchaseDate: moment('2015-03-09').utcOffset(0, true).toDate(),
       lifts: [],
       isUnUsed: false,
       balance: 0,
-      syncDate: new Date('2017-01-09'),
-      updateDate: new Date('2017-01-09')
+      syncDate: moment('2017-01-09').utcOffset(0, true).toDate(),
+      updateDate: moment('2017-01-09').utcOffset(0, true).toDate()
     }
     const onRemoveSkipass = jest.fn()
     const muiTheme = getMuiTheme()
