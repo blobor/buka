160cf384f0bce32297fdbc1eb9f82fb7828fa9a5
Bogdan Lobor <b.lobor@outlook.com>
2017-01-12 09:03:00 +0200

    [gh-49]: mock date formatter function

--- __tests__/skipasses/SkipassCard.js
+++ __tests__/skipasses/SkipassCard.js
@@ -7,18 +7,22 @@ import { MuiThemeProvider } from 'material-ui/styles'
 import { getMuiTheme } from '../../src/app/core/mui-theme'
 import SkipassCard from '../../src/app/skipasses/SkipassCard.component'
 
+jest.mock('../../src/app/helpers/date', () => ({
+  getAdoptedDateString: date => date.toISOString()
+}))
+
 describe('SkipassCard', () => {
   it('should renders correctly', () => {
     const skipass = {
       cardNumber: '01-2167-30-92545',
       ticketNumber: '01-1614 7133 5345 3531 0476-4',
       name: '5 днів СЕЗОН (ВСЕF)',
-      purchaseDate: moment('2015-03-09').utcOffset(0, true).toDate(),
+      purchaseDate: moment.utc('2015-03-09').toDate(),
       lifts: [],
       isUnUsed: false,
       balance: 0,
-      syncDate: moment('2017-01-09').utcOffset(0, true).toDate(),
-      updateDate: moment('2017-01-09').utcOffset(0, true).toDate()
+      syncDate: moment.utc('2017-01-09').toDate(),
+      updateDate: moment.utc('2017-01-09').toDate()
     }
     const onRemoveSkipass = jest.fn()
     const muiTheme = getMuiTheme()
