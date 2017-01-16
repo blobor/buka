64b2982be9ef93c6be1bcf441ed3b14bc0e8ef6b
Bogdan Lobor <b.lobor@outlook.com>
2017-01-16 18:38:09 +0200

    [gh-48]: added `toggleDeveloperMode`

--- src/app/about/index.js
+++ src/app/about/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { List, ListItem, Divider } from 'material-ui'
 
-const About = ({ appVersion }) => {
+import { toggleDeveloperMode } from '../core/actions/action-creators/app'
+
+const About = ({ appVersion, enableDeveloperMode }) => {
   return (
     <article className='about__container'>
-
       <List>
-        <ListItem primaryText='App version' secondaryText={appVersion} />
+        <ListItem primaryText='App version' secondaryText={appVersion} onTouchTap={enableDeveloperMode} />
         <Divider />
       </List>
     </article>
@@ -22,7 +23,15 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    enableDeveloperMode: () => {
+      dispatch(toggleDeveloperMode(true))
+    }
+  }
+}
+
 export {
   About
 }
-export default connect(mapStateToProps)(About)
+export default connect(mapStateToProps, mapDispatchToProps)(About)
