4f91dcff24dc47c8355f7902a9d7013f656ebe9b
Bogdan Lobor <b.lobor@outlook.com>
2017-01-16 18:38:09 +0200

    [gh-48]: added `toggleDeveloperMode`

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { AppBar } from 'material-ui'
 import TestCardNumber from './TestCardNumber'
 
-const Header = ({ testCardNumberChange }) => {
+const Header = ({ developerMode, testCardNumberChange }) => {
+  const rightElement = developerMode ? <TestCardNumber onChange={testCardNumberChange} /> : null
+
   return (
     <header className='app-header'>
       <AppBar
         className='app-header__appbar'
         showMenuIconButton={false}
-        iconElementRight={
-          <TestCardNumber onChange={testCardNumberChange} />
-        } />
+        iconElementRight={rightElement} />
     </header>
   )
 }
