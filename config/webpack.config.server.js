8258df457162b21411f893229bf086bc4b9be82b
Bogdan Lobor <b.lobor@outlook.com>
2017-03-12 14:30:57 +0200

    Minify js in case production build

--- config/webpack.config.server.js
+++ config/webpack.config.server.js
@@ -1,43 +1,50 @@
 'use strict'
 
 const { resolve, join } = require('path')
+const { getIfUtils, removeEmpty } = require('webpack-config-utils')
 const webpack = require('webpack')
+const BabiliPlugin = require("babili-webpack-plugin");
 
 const root = resolve(__dirname, '..')
 const distDir = 'dist-server'
 const buildDirPath = join(root, distDir)
 
-module.exports = {
-  context: root,
-  entry: {
-    server: './src/server.js'
-  },
-  output: {
-    publicPath: `${distDir}/`,
-    path: buildDirPath,
-    libraryTarget: 'commonjs2',
-    filename: '[name].js'
-  },
-  target: 'node',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: true
+module.exports = (env = (process.env.NODE_ENV || 'development')) => {
+  const { ifProduction } = getIfUtils(env)
+
+  return {
+    context: root,
+    entry: {
+      server: './src/server.js'
+    },
+    output: {
+      publicPath: `${distDir}/`,
+      path: buildDirPath,
+      libraryTarget: 'commonjs2',
+      filename: '[name].js'
+    },
+    target: 'node',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }, {
+          test: /\.hbs$/,
+          loader: 'file-loader',
+          options: {
+            name: 'views/[name].[ext]'
+          }
         }
-      }, {
-        test: /\.hbs$/,
-        loader: 'file-loader',
-        options: {
-          name: 'views/[name].[ext]'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
+      ]
+    },
+    plugins: removeEmpty([
+      new webpack.NoEmitOnErrorsPlugin(),
+      ifProduction(new BabiliPlugin())
+    ])
+  }
 }
